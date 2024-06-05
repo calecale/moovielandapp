52b8131f6348e21056e5301c0ea2d7e7f76d0eef
Aleksandra <maleksandrabiz@gmail.com>
2024-06-05 21:21:03 +0200

    using useEffect hook

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
+
+
+//created an api url
+const API_URL = 'https://www.omdbapi.com?apikey=c3721b49';
 
 const App = () => {
+
+    //f-ja za pretrazivanje filmova po nazivu
+    const searchMoovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+    }
+
+    useEffect(() => {
+
+    }, []); //prima prazan niz ako zelimo da je pozovemo samo u startu
     return (
         <h1> App </h1>
     );
